672aad5ed96a4c2fdf2e41864ef6fc4008e4cc10
KhuongVAnh <anhkvwork@gmai.com>
2026-04-18 13:28:57 +0700

    feat(api): unify success/error payload format; implement JWT auth and role guard

---
+++ backend/catalog_service/src/types/express.d.ts
@@ -0,0 +1,25 @@
+import "express";
+
+/**
+Đây là file mở rộng type cho Express Request.
+Bình thường req.authUser không tồn tại trong type gốc của Express.
+Sau khi khai báo augmentation, TypeScript hiểu rằng middleware auth sẽ gắn thêm authUser vào request.
+Nếu bỏ file này:
+Code runtime vẫn có thể chạy.
+Nhưng TypeScript sẽ báo lỗi kiểu “Property authUser does not exist on type Request”.
+File này giúp chúng ta tránh lỗi kiểu đó, và có thể truy cập req.authUser một cách an toàn trong code, không có thì trả về undifined.
+ */
+declare global {
+    namespace Express {
+        interface Request {
+            authUser?: {
+                userId: bigint;
+                email: string;
+                fullName: string;
+                role: string;
+            };
+        }
+    }
+}
+
+export { };
