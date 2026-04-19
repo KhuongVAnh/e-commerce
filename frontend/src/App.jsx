97ca3a19383bd09ebc6e0c17089dbabbfe9a4fe8
vanthang10tin <vanthang10tin@gmail.com>
2026-04-19 20:21:59 +0700

    feat: cấu hình route cho Seller Hub và thêm các trang placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 import AdminDashboard from './pages/admin/AdminDashboard';
 import SellerDashboard from './pages/seller/SellerDashboard';
+import ShopForm from './pages/seller/ShopForm';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
@@ -40,6 +41,9 @@ function App() {
         <Route element={<ProtectedRoute allowedRoles={['seller', 'admin']} />}>
           <Route path="/seller" element={<DashboardLayout roleTitle="Seller" />}>
             <Route index element={<SellerDashboard />} />
+            
+            {/* /shop/settings sẽ mở Form  */}
+            <Route path="shop/settings" element={<ShopForm />} />
           </Route>
         </Route>
 
